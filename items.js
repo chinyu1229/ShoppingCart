d67989f771504a6ed91eca332d7102de8fe523dd
Chinyu <chinyu0916@gmail.com>
2021-11-27 17:55:45 +0800

    set-mongodb

---
+++ items.js
@@ -0,0 +1,9 @@
+//use mongoose
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/cart', { useNewUrlParser: true, useUnifiedTopology: true })
+
+var Schema = mongoose.Schema
+
+
+
+
