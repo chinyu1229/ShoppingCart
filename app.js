106606086336eefb20867c8274fdc2557a1032d2
Chinyu <chinyu0916@gmail.com>
2021-11-27 18:25:02 +0800

    add require

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var express = require('express')
+var router = require('./router')
+var bodyParser = require('body-parser')
 var app = express()
 
 app.use('/public/', express.static('./public'))
 app.engine('html', require('express-art-template'))
 app.use('/node_modules', express.static('./node_modules/'))
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(router)
 
 app.listen(3001, function () {
     console.log('server running')
