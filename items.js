4a566ba368b79efc0a40853c71b4310353754c4c
Chinyu <chinyu0916@gmail.com>
2021-11-27 18:26:12 +0800

    conncet-mongoose

--- items.js
+++ items.js
@@ -1,9 +1,27 @@
 //use mongoose
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/cart', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/circus', { useNewUrlParser: true, useUnifiedTopology: true })
 
 var Schema = mongoose.Schema
 
+var itemSchema = new Schema({
+    itemName: {
+        type: String,
+        required: true
+    },
+    shopName: {
+        type: Number,
+        enum: [0, 1],
+        default: 0
+    },
+    website: {
+        type: Number
+    },
+    price: {
+        type: String
+    }
+})
 
+module.exports = mongoose.model('shoppingCart', itemSchema)
 
 
