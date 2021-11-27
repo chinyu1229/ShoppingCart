c042afb5487ddd48e6588dba591719db376d4a91
Chinyu <chinyu0916@gmail.com>
2021-11-27 21:13:49 +0800

    Edit-function-add

--- router.js
+++ router.js
@@ -6,7 +6,6 @@ const { runInNewContext } = require('vm')
 var router = express.Router()
 
 router.get('/home', function (req, res) {
-
     Item.find(function (err, items) {
         if (err) {
             return res.status(500).send('server error')
@@ -22,6 +21,44 @@ router.get('/home', function (req, res) {
             , items: items//JSON.parse(data).items
 
         })
+    })
+
+})
+
+//新增商品 get/post
+router.get('/items/new', function (req, res) {
+    res.render('new.html')
+})
+router.post('/items/new', function (req, res) {
+    new Item(req.body).save(function (err) {
+        if (err) {
+            return res.status(500).send('Server error')
+
+        }
+        res.redirect('/home')
+    })
+
+})
+
+//編輯修改 get/post
+router.get('/items/edit', function (req, res) {
+    Item.findById(req.query.id, function (err, item) {
+        if (err) {
+            return res.status(500).send('server error')
+        }
+        res.render('edit.html', {
+            item: item
+        })
+    })
+})
+router.post('/items/edit', function (req, res) {
+    var id = req.body.id
+
+    Item.findByIdAndUpdate(id, req.body, function (err) {
+        if (err) {
+            return res.status(500).send('server error')
+        }
+        res.redirect('/home')
     })
 })
 
